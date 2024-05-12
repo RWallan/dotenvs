c0a1bf653f953903af95d7ef56e00839bd043544
~RWallan~ <~3am.richarwallan@gmail.com~>
2024-05-11 23:25:59 -0300

    Post install

---
+++ post_install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+echo "Pacotes básicos"
+sudo pacman -S --noconfirm neovim git neofetch
+
+echo "Melhorando o pacman"
+sudo nvim -c '%s/#Color/Color/g' -c '%s/#ParallelDownloads = 5/ParallelDownloads = 10/g' -c 'wq' /etc/pacman.conf
+
+echo "Atualizando sistema"
+sudo pacman -Syu --noconfirm
+
+echo "Instalando firmware e firewall"
+sudo pacman -S --noconfirm gufw fwupd
+sudo ufw enable
+fwupdmgr get-updates
+fwupdmgr update
+
+echo "Configurando bluetooth"
+sudo systemctl start bluetooth.service --now
+
+echo "Instalando codecs"
+sudo pacman -S --noconfirm ffmpeg gst-plugin-ugly gst-plugin-good gst-plugin-base gst-plugin-bad gst-libav gstreamer
+
+echo "Instalando compatibilidade com NTFS"
+sudo pacman -S --noconfirm ntfs-3g
+
+echo "Instalando yay"
+pacman -Sy --needed --noconfirm base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -Y --gendb
+
+echo "Programming"
+yay -S --noconfirm tmux beekeeper-studio-bin docker act github-cli
+yay -S --noconfirm pyenv tk pipx
+yay -S node npm
+
+echo "Python"
+yay -S --noconfirm python-pipx
+pyenv install 3.10:latest 3.11:latest 3.12:latest
+pipx install poetry
+
+echo "Configurando Poetry"
+poetry config virtualenvs.prompt "{project_name"
+poetry config virtualenvs.in-project true
+
+echo "Outras coisas"
+yay -S --noconfirm marktext-bin libreoffice-fresh libreoffice-fresh-pt-br telegram-desktop barrier unzip
+
+echo "Fim"

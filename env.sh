4c661e4d7570917be5c45012ec2dbcd26d821eea
~RWallan~ <~3am.richarwallan@gmail.com~>
2024-05-12 00:43:00 -0300

    typo

--- env.sh
+++ env.sh
@@ -3,4 +3,4 @@
 cp -r bash/.bashr* ~/
 cp -r nvim/ ~/.config/
 cp -r tmux/ ~/.config/
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugin/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm

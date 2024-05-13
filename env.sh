efebe8737cfedd87b1f1a41a669579304aa7736c
RWallan <3am.richardwallan@gmail.com>
2024-05-13 00:45:02 -0300

    adicionado copia de config do xterm

--- env.sh
+++ env.sh
@@ -4,4 +4,4 @@ cp -r bash/.bashr* ~/
 cp -r nvim/ ~/.config/
 cp -r .poshthemes/ ~/
 cp -r tmux/ ~/.config/
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+cp .Xresources ~/.Xresources

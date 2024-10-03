20a58793727d032ab7383e1cc84534425ebd8564
RWallan <3am.richardwallan@gmail.com>
2024-10-03 11:15:16 -0300

    feat: render md elements and quality of life

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,12 +1,27 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-    config = function()
-        local keymap = vim.keymap
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+        config = function()
+            local keymap = vim.keymap
 
-        keymap.set("n", "<leader>ms", "<cmd>MarkdownPreview<CR>", { desc = "Starts the preview" })
-        keymap.set("n", "<leader>mS", "<cmd>MarkdownPreviewStop<CR>", { desc = "Stops the preview" })
-    end,
+            keymap.set("n", "<leader>ms", "<cmd>MarkdownPreview<CR>", { desc = "Starts the preview" })
+            keymap.set("n", "<leader>mS", "<cmd>MarkdownPreviewStop<CR>", { desc = "Stops the preview" })
+        end,
+    },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {},
+        dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    },
+    {
+        "ixru/nvim-markdown",
+    }
 }
+

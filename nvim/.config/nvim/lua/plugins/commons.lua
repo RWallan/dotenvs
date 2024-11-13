c993cab00d2a2b20511780906c6b15dc5ce8f559
RWallan <3am.richardwallan@gmail.com>
2024-11-13 18:49:48 -0300

    feat: removing unecessary plugins

--- nvim/.config/nvim/lua/plugins/commons.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "nvim-lua/plenary.nvim",       -- lua functions that many plugins use
-    "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-    "famiu/bufdelete.nvim",
-    "tpope/vim-surround"
-}

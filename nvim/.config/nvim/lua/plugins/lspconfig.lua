c0f8af14d459a47411e222df57d13f2b323439e8
RWallan <3am.richardwallan@gmail.com>
2025-02-06 12:34:38 -0300

    fix: fixing markdown lint error and some improves

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -151,6 +151,7 @@ return { -- LSP Plugins
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
+				marksman = {},
 				gopls = {},
 				pyright = {},
 				rust_analyzer = {},
@@ -195,10 +196,13 @@ return { -- LSP Plugins
 				"ruff",
 				"prettierd",
 				"gofumpt",
+				"markdownlint",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			require("mason-lspconfig").setup({
+				ensure_installed = ensure_installed,
+				automatic_installation = true,
 				handlers = {
 					function(server_name)
 						local server = servers[server_name] or {}
